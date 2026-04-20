3c9a2e95e6709a63c2bcdab4371bbff571919d67
Marcelo Tomas Duo <duomarcelo5@gmail.com>
2026-04-20 00:20:58 -0300

    puse info

---
+++ clase 4/03.js
@@ -0,0 +1,25 @@
+const nombre = 'marcelo'
+
+if (nombre == 'marcelo') {
+    console.log('hola marcelo')
+}
+
+
+
+if (nombre == 'marcelo') {
+    console.log('hola marcelo')
+}else{9
+    console.log("hola desconocido")
+}
+
+
+
+if (nombre == 'marcelo') {
+    console.log('hola marcelo')
+
+}else if (nombre == 'juan') {
+    console.log("hola juan")
+
+}else {
+    console.log('hola desconocido')
+}
