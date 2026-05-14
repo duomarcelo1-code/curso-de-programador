015d9f33c7e840588c87375210edc4c72c60ce55
Marcelo Tomas Duo <duomarcelo5@gmail.com>
2026-05-13 22:24:12 -0300

    CARTA

--- clase 4/05-for.js
+++
@@ -1,6 +0,0 @@
-const frutas =['bananas', 'manzanas', 'paltas', 'naranjas', 'mandarinas','bananas', 'manzanas', 'paltas', 'naranjas', 'mandarinas'];
-
-
-for(let i =0 ; i < frutas.length; i++ ) {
-    console.log(`Tenemos ${frutas[i]}`)
-}
--- clase 4/06-for2.js
+++
@@ -1,8 +0,0 @@
-const numeros = [1, 34, 45, 67, 78];
-let suma = 0
-
-for (let i = 0; i < numeros.length; i++) {
-    suma += numeros[i]
-}
-
-console.log(suma)
