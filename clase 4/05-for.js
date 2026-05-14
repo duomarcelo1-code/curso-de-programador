f4f5f2ea74fb944b2d405b04662cfe5c186a8092
Marcelo Tomas Duo <duomarcelo5@gmail.com>
2026-05-14 01:14:20 -0300

    carta

---
+++ clase 4/05-for.js
@@ -0,0 +1,6 @@
+const frutas =['bananas', 'manzanas', 'paltas', 'naranjas', 'mandarinas','bananas', 'manzanas', 'paltas', 'naranjas', 'mandarinas'];
+
+
+for(let i =0 ; i < frutas.length; i++ ) {
+    console.log(`Tenemos ${frutas[i]}`)
+}
